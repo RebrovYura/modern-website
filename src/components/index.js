50b712ee57bed0d4e48a6fc9cf0211d6cf10b3f6
Yuri Rebrov <yurirebrov01@gmail.com>
2023-02-05 19:09:24 +0300

    folder stucture

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './components/Billing';
+import Business from './components/Business';
+import Button from './components/Button';
+import CardDeal from './components/CardDeal';
+import Clients from './components/Clients';
+import CTA from './components/CTA';
+import FeedbackCard from './components/FeedbackCard';
+import Footer from './components/Footer';
+import GetStarted from './components/GetStarted';
+import Hero from './components/Hero';
+import Navbar from './components/Nabvar';
+import Stats from './components/Stats';
+import Testimonials from './components/Testimonials';
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials
+};
